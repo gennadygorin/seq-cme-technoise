a63a95a86d3fd0389723c836efbfd2dce02dc44a
gennadygorin <43971338+gennadygorin@users.noreply.github.com>
2022-05-25 18:22:38 -0700

    Update cme_toolbox.py

--- cme_toolbox.py
+++ cme_toolbox.py
@@ -17,7 +17,12 @@ from preprocess import *
 class CMEModel:
     def __init__(self,bio_model,seq_model,quad_method='fixed_quad',fixed_quad_T=10,quad_order=60,quad_vec_T=np.inf):
         self.bio_model = bio_model
-        self.seq_model = seq_model
+        self.available_biomodels = ('Delay','Bursty','Extrinsic','Constitutive','CIR')
+        self.available_seqmodels = ('None','Bernoulli','Poisson')
+        if (seq_model == 'None') or (seq_model is None) or (seq_model == 'Null'):
+            self.seq_model = 'None'
+        else: 
+            self.seq_model = seq_model
         self.set_integration_parameters(fixed_quad_T,quad_order,quad_vec_T,quad_method)
 
     def set_integration_parameters(self,fixed_quad_T,quad_order,quad_vec_T,quad_method):
@@ -27,16 +32,18 @@ class CMEModel:
         self.quad_method = quad_method
 
     def get_log_name_str(self):
-        if self.bio_model == 'Poisson':
-            raise ValueError('I still need to implement this one.')
+        if self.bio_model == 'Constitutive':
+            return (r'$\log_{10} \beta$',r'$\log_{10} \gamma$')
         elif self.bio_model == 'Delay':
             return (r'$\log_{10} b$',r'$\log_{10} \beta$',r'$\log_{10} \tau^{-1}$')
         elif self.bio_model == 'Bursty':
             return (r'$\log_{10} b$',r'$\log_{10} \beta$',r'$\log_{10} \gamma$')
-        elif self.bio_model == 'Extrinsic': #constitutive production with extrinsic noise
-            raise ValueError('I still need to implement this one.')
+        elif self.bio_model == 'Extrinsic': 
+            return (r'$\log_{10} b$',r'$\log_{10} \beta$',r'$\log_{10} \gamma$')
+        elif self.bio_model == 'CIR': 
+            return (r'$\log_{10} b$',r'$\log_{10} \beta$',r'$\log_{10} \gamma$')
         else:
-            raise ValueError('Please select a biological noise model from {Poisson}, {Bursty}, {Extrinsic}, {Delay}.')
+            raise ValueError('Please select a biological noise model from {}.'.format(self.available_biomodels))
 
 
     def eval_model_pss(self,p,limits,samp=None):
@@ -50,10 +57,10 @@ class CMEModel:
                 u_ = np.exp((10**samp[i])*u_)-1
             elif self.seq_model == 'Bernoulli': #it might be better to have this one in terms of a positive optimizable value
                 u_ *= samp[i]
-            elif self.seq_model == 'None' or self.seq_model is None:
+            elif self.seq_model == 'None':
                 pass
             else:
-                raise ValueError('Please select a technical noise model from {Poisson}, {Bernoulli}, {None}.')
+                raise ValueError('Please select a technical noise model from {}.'.format(self.available_seqmodels))
             u.append(u_)
         g = np.meshgrid(*[u_ for u_ in u], indexing='ij')
         for i in range(len(mx)):
@@ -70,7 +77,8 @@ class CMEModel:
     def eval_model_pgf(self,p,g):
         p = 10**p #these are going to have different interpretations for different models. Should we harmonize them?
         if self.bio_model == 'Poisson': #constitutive production
-            gf = g[0]*p[0] + g[1]*p[1]
+            beta,gamma = p
+            gf = g[0]/beta + g[1]/gamma
         elif self.bio_model == 'Bursty': #bursty production
             b,beta,gamma = p
             fun = lambda x: self.burst_intfun(x,g,b,beta,gamma)
@@ -83,15 +91,44 @@ class CMEModel:
             else:
                 raise ValueError('Please use one of the specified quadrature methods.')
         elif self.bio_model == 'Extrinsic': #constitutive production with extrinsic noise
-            raise ValueError('I still need to implement this one.')
+            alpha,beta,gamma = p
+            gf = -alpha * np.log(1 - g[0]/beta - g[1]/gamma)
         elif self.bio_model == 'Delay': #bursty production with delayed degradation
             b,beta,tauinv = p
             tau = 1/tauinv
             U  = g[1] + (g[0]-g[1])*np.exp(-beta*tau)
             gf = -1/beta * np.log(1-b*U) + 1/beta/(1-b*g[1]) * np.log((b*U-1)/(b*g[0]-1)) + tau * b*g[1]/(1-b*g[1])
+        elif self.bio_model == 'CIR': #CIR-like:
+            b,beta,gamma = p
+            fun = lambda x: self.cir_intfun(x,g,b,beta,gamma)
+            if self.quad_method=='quad_vec':
+                T = self.quad_vec_T*(1/beta + 1/gamma + 1)
+                gf = scipy.integrate.quad_vec(fun,0,T)[0]
+            elif self.quad_method=='fixed_quad':
+                T = self.fixed_quad_T*(1/beta + 1/gamma + 1)
+                gf = scipy.integrate.fixed_quad(fun,0,T,n=self.quad_order)[0]
+            else:
+                raise ValueError('Please use one of the specified quadrature methods.')
+            gf /= 2
         else:
-            raise ValueError('Please select a biological noise model from {Poisson}, {Bursty}, {Extrinsic}, {Delay}.')
+            raise ValueError('Please select a biological noise model from {}.'.format(self.available_biomodels))
         return gf #this is the log-generating function
+    
+    def cir_intfun(self,x,g,b,beta,gamma):
+        """
+        Computes the IG-driven integrand at time x.
+        """
+        if np.isclose(beta,gamma): #compute prefactors for the ODE characteristics.
+            c_1 = g[0] #nascent
+            c_2 = x*beta*g[1]
+        else:
+            f = beta/(beta-gamma)
+            c_2 = g[1]*f
+            c_1 = g[0] - c_2
+
+        U = b * (np.exp(-beta*x)*c_1 + np.exp(-gamma*x)*c_2)
+        return 1-np.sqrt(1-4*U)
+
 
     def burst_intfun(self,x,g,b,beta,gamma):
         """
@@ -115,38 +152,55 @@ class CMEModel:
         lower bound and upper bound are harmonized with optimization routine and input as log10.
         """
 
-        lb = 10**lb_log
-        ub = 10**ub_log
+        # lb = 10**lb_log
+        # ub = 10**ub_log
+        if self.seq_model == 'Poisson':
+            samp = 10**samp
         
-        if self.bio_model == 'Bursty':
+        if self.bio_model == 'Bursty' or self.bio_model == 'CIR':
             b = moments['U_var'] / moments['U_mean'] - 1
-            if self.seq_model == 'Poisson':
-                samp = 10**samp
-                b = b / samp[0] - 1
-            elif self.seq_model == 'None':
-                samp = [1,1]
-            elif self.seq_model == 'Bernoulli':
-                raise ValueError('I still need to implement this one.')
 
-            b = np.clip(b,lb[0],ub[0])
-            beta = np.clip(b * samp[0] / moments['U_mean'], lb[1], ub[1])
-            gamma = np.clip(b * samp[1] / moments['S_mean'], lb[2], ub[2])
-            x0 = np.log10(np.asarray([b,beta,gamma]))
-        elif self.bio_model == 'Delay':
+            if self.seq_model == 'Bernoulli':
+                b /= samp[0]
+            elif self.seq_model == 'Poisson':
+                b = b/samp[0] - 1
+
+            beta = b / moments['U_mean']
+            gamma = b / moments['S_mean']
+            x0 = np.asarray([b,beta,gamma])
+
+        elif self.bio_model == 'Delay':            
             b = moments['U_var'] / moments['U_mean'] - 1
-            if self.seq_model == 'Poisson':
-                samp = 10**samp
-                b = b / samp[0] - 1
-                # log.warning('I am not totally sure about this one.')
-            elif self.seq_model == 'None':
-                samp = [1,1]
-            elif self.seq_model == 'Bernoulli':
-                raise ValueError('I still need to implement this one.')
 
-            b = np.clip(b,lb[0],ub[0])
-            beta = np.clip(b * samp[0] / moments['U_mean'], lb[1], ub[1])
-            tauinv = np.clip(b * samp[1] / moments['S_mean'], lb[2], ub[2])
-            x0 = np.log10(np.asarray([b,beta,tauinv]))
+            if self.seq_model == 'Bernoulli':
+                b /= samp[0]
+            elif self.seq_model == 'Poisson':
+                b = b/samp[0] - 1
+
+            beta = b / moments['U_mean']
+            tauinv = b / moments['S_mean']
+            x0 = np.asarray([b,beta,tauinv])
+
+        elif self.bio_model == 'Constitutive':
+            beta = 1 / moments['U_mean']
+            gamma = 1 / moments['S_mean']
+            x0 = np.asarray([beta,gamma])
+
+        elif self.bio_model == 'Extrinsic':
+            if self.seq_model == 'Poisson':
+                alpha = moments['U_mean']**2/(moments['U_var'] - moments['U_mean']*(1+samp[0]))
+            else:
+                alpha = moments['U_mean']**2/(moments['U_var'] - moments['U_mean'])
+
+            beta = alpha / moments['U_mean']
+            gamma = alpha / moments['S_mean']
+            x0 = np.asarray([alpha,beta,gamma])
         else:
-            raise ValueError('I still need to implement the other models.')
+            raise ValueError('Please select from existing models.')
+
+        if self.bio_model == 'Constitutive':
+            b *= samp
+        else:
+            b[1:] = b[1:] * samp
+
         return x0
